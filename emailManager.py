a9e77ccac95d5ca2ac4c0088cab2b5098b97d4dd
Dimitri Berardi <Dimitri.Berardi@gmail.com>
2021-08-16 22:24:12 -0400

    Full Functionality

--- emailManager.py
+++ emailManager.py
@@ -1,7 +1,35 @@
-from emailUtils import Mailbox
+from imap_tools import MailBox
+from tinydb import TinyDB, Query
+from credentialUtils import Credentials
+import logging
 
-mailbox = Mailbox.setFolderUnsub()
 
-mailbox.printAllMessages()
+logging.basicConfig(level=logging.DEBUG)
+db = TinyDB('db.json')
 
-print("Done")
+credentials = Credentials.get_credentials()
+logging.info("Successfully retrieved credentials")
+
+mailbox = MailBox('imap.gmail.com').login(credentials.username, credentials.password,
+                                          initial_folder="Unsubscribe")
+
+logging.info("Logged into Gmail successfully")
+
+for msg in mailbox.fetch():
+    if db.contains(Query().email == msg.from_):
+        logging.info(f"{msg.from_} already in DB")
+    else:
+        logging.info(f"Adding {msg.from_} to DB")
+        db.insert({'email': msg.from_})
+
+mailbox.delete([msg.uid for msg in mailbox.fetch()])
+
+mailbox.folder.set("INBOX")
+
+for msg in mailbox.fetch():
+    logging.info(f"Checking email: {msg.from_}")
+    if db.contains(Query().email == msg.from_):
+        logging.info(f"Deleting email")
+        mailbox.delete(msg.uid)
+
+logging.info("Operation completed successfully")
--- emailUtils.py
+++ emailUtils.py
@@ -1,7 +1,6 @@
-from beeprint import pp
 from imap_tools import MailBox
-
 from credentialUtils import Credentials
+import logging
 
 
 class Mailbox:
@@ -9,17 +8,27 @@ class Mailbox:
         credentials = Credentials.get_credentials()
         self.mailbox = MailBox('imap.gmail.com').login(credentials.username, credentials.password,
                                                        initial_folder=initial_folder)
+        logging.info('Mailbox created')
+        self.messageList = self.mailbox.fetch()
 
     @classmethod
-    def setFolderUnsub(cls):
+    def Unsub(cls):
         return cls("Unsubscribe")
 
     @classmethod
-    def setFolderInbox(cls):
+    def Inbox(cls):
         return cls("INBOX")
 
+    def getSenders(self):
+        senders = []
+
+        for msg in self.messageList:
+            senders.append(msg.from_)
+
+        return senders
+
     def printAllMessages(self):
-        for msg in self.mailbox.fetch():
+        for msg in self.messageList:
             message = Message(msg)
             message.print()
 
@@ -37,4 +46,3 @@ class Message:
 
     def print(self):
         print(self.from_, " -- ", self.subject)
-        pp(self.flags)
